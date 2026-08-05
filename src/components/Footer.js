7745cd5e6005ab40f9107d63d1f04147d89ad8f1
Akhilesh Kumar Sahu <118347309+mrakhil456@users.noreply.github.com>
2026-08-06 00:29:33 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
-        <p>&copy; {new Date().getFullYear()} Akhilesh Developer. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} Uday Developer. All rights reserved.</p>
       </div>
     </footer>
   );
